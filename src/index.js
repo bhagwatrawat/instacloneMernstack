09995bc10f10041e1a89895304edd27aeeecfb65
Bhagwat Rawat <bhagwat12rawat@gmail.com>
2020-10-26 22:58:48 +0530

    chat

--- src/index.js
+++ src/index.js
@@ -27,10 +27,10 @@ io.on('connection',(socket)=>{
 })
 
 if(process.env.NODE_ENV=="production"){
-  app.use(express.static('instaclonereact/build'))
+  app.use(express.static('client/build'))
   const path=require('path')
   app.get("*",(req,res)=>{
-    res.sendFile(path.resolve(__dirname,'instaclonereact','build','index.html'))
+    res.sendFile(path.resolve(__dirname,'client','build','index.html'))
   })
 }
 server.listen(PORT,()=>{
